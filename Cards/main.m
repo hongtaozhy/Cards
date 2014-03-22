cb0c83e4cfa356b6845ad5ec3988497e8b0ec91d
Alfie Hanssen <alfiehanssen@gmail.com>
2014-03-22 19:47:18 -0400

    Initial Commit

---
+++ Cards/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cards
+//
+//  Created by Alfred Hanssen on 3/22/14.
+//  Copyright (c) 2014 Alfred Hanssen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
